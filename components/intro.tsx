243383a991b35d01a0bfaf2f8b7a72bc416911bd
Shubham Soni <shubhamsonicse@gmail.com>
2025-07-22 21:50:58 +0530

    Updated Profle Image

--- components/intro.tsx
+++ components/intro.tsx
@@ -31,13 +31,13 @@ export default function Intro() {
             }}
           >
             <Image
-              src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?fit=crop&w=368&h=368&q=100"
-              alt="Ricardo portrait"
+              src="/profile.jpg"
+              alt="Shubham Soni"
               width="192"
               height="192"
               quality="95"
               priority={true}
-              className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
+              className="h-32 w-32 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
             />
           </motion.div>
 
