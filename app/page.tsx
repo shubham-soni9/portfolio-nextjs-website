250d47b780ff5f12102d51a6fdf9b8d9762084f7
Shubham Soni <shubhamsonicse@gmail.com>
2025-08-11 18:46:59 +0530

    Added

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,11 @@ import Contact from "@/components/contact";
 import Experience from "@/components/experience";
 import Intro from "@/components/intro";
 import Projects from "@/components/projects";
-import SectionDivider from "@/components/section-divider";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center px-4">
       <Intro />
-      <SectionDivider />
       <Projects />
       <Experience />
       <Contact />
--- components/projects.tsx
+++ components/projects.tsx
@@ -9,7 +9,7 @@ export default function Projects() {
   const { ref } = useSectionInView("Projects", 0.5);
 
   return (
-    <section ref={ref} id="projects" className="scroll-mt-28 mb-28">
+    <section ref={ref} id="projects" className="mt-10 scroll-mt-28 mb-28">
       <SectionHeading>My projects</SectionHeading>
       <Project />
     </section>
